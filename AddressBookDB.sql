c151a1a0ccd61a71d3ba8908a9e23b8131b4cdf8
RohanBPatil <rohanpatil14218@gmail.com>
2020-10-30 17:20:48 +0530

    Added : Ability to create create table addressBookTable

--- AddressBookDB.sql
+++ AddressBookDB.sql
@@ -2,3 +2,18 @@
 create database AddressBookService;
 show databases;
 use AddressBookService;
+
+# UC 2
+create table addressBookTable
+(
+firstName varchar(100) NOT NULL,
+lastName varchar(100) not null,
+address varchar(200) not null,
+city varchar(100) not null,
+state varchar(100) not null,
+zip numeric(6) not null,
+phone numeric(10) not null,
+email varchar(100) not null,
+primary key (firstName, lastName)
+);
+describe addressBookTable;
